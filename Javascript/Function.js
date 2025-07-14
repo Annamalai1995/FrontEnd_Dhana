773a4928dac7d1cf248d1cbba5305d33c284cb03
Annamalai1995 <s.annamalai1995@gmail.com>
2025-07-14 09:54:40 +0530

    fucntion array

---
+++ Javascript/Function.js
@@ -0,0 +1,17 @@
+function Dhana()
+{
+    alert("Sona College Bsc Computer Science")
+}
+function add(alpha,beta)
+{
+    let sum=alpha+beta
+    document.writeln("Alpha value"+alpha+"Beta value"+beta+"Result"+sum) 
+
+}
+let name=(print)=>
+{
+
+    console.log("Place"+print)
+
+
+}
